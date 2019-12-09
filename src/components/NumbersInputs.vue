266a97a66c1a9008f97d0baa0adc7972df9276ed
silencer903 <zapara04@rambler.ru>
2019-12-10 01:15:33 +0500

    Сделал инпуты и базовый css к ним, написал каркасы функций для кнопок

---
+++ src/components/NumbersInputs.vue
@@ -0,0 +1,15 @@
+<template>
+    <input class="number-input" type="number">
+</template>
+
+<script>
+    export default {
+        name: "numbers-inputs"
+    }
+</script>
+
+<style scoped lang="scss">
+.number-input:not(active){
+    color: red;
+}
+</style>
